597ca5e1c06ef5bfd0e726fa5f4f4589e7d88384
Alexander Kravets <a@kra.vc>
2015-04-30 08:23:37 -0700

    add safe protection for character helper;

--- lib/mongoid/character.rb
+++ lib/mongoid/character.rb
@@ -11,12 +11,12 @@ module Mongoid
 
     def _list_item_title
       first_non_id_attribute = self.attributes.keys.select { |k| k != '_id' }.first
-      self[first_non_id_attribute]
+      self[first_non_id_attribute].to_s
     end
 
 
     def _list_item_subtitle
-      'created ' + time_ago_in_words(created_at) + ' ago' if created_at
+      "created #{ time_ago_in_words(created_at) } ago" if created_at
     end
 
 
